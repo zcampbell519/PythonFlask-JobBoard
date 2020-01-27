e996665273c320e933d4ee85397b3f1c34433a38
Zac <zcampbell@outlook.com>
2020-01-27 14:19:17 -0700

    mod7

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,6 @@
 import sqlite3
-from flask import Flask, g 
-from flask import render_template
+from flask import Flask, g, render_template, request, redirect, url_for
+import datetime
 
 
 PATH = 'db/jobs.sqlite'
@@ -47,3 +47,15 @@ def employer(employer_id):
     jobs=execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?',[employer_id])
     reviews=execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?',[employer_id])
     return render_template('employer.html',employer=employer,jobs=jobs, reviews=reviews)
+
+@app.route('/employer/<employer_id>/review',methods=('GET','POST'))
+def review(employer_id):
+    if request.method=='POST':
+        review=request.form['review']
+        rating=request.form['rating']
+        title=request.form['title']
+        status=request.form['status']
+        date=datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',(review,rating, title, date, status, employer_id),commit=True)
+        return redirect(url_for('employer',employer_id=employer_id))
+    return render_template('review.html',employer_id=employer_id)
